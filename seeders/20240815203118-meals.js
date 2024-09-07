78ede23ba2505d4c581fb0fd9337fac1d5225760
Juliano Almada <almadaj@edu.unifor.br>
2024-09-07 13:59:42 -0300

    chore: icones de Meals renderizam icones padrões

--- seeders/20240815203118-meals.js
+++ seeders/20240815203118-meals.js
@@ -1,62 +1,62 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('meals', [
+    return queryInterface.bulkInsert("meals", [
       {
-        icon: 'breakfast_icon.png',
-        name: 'Breakfast',
+        icon: "https://cdn-icons-png.flaticon.com/256/5024/5024555.png",
+        name: "Breakfast",
         userId: 1,
       },
       {
-        icon: 'lunch_icon.png',
-        name: 'Lunch',
+        icon: "https://cdn-icons-png.flaticon.com/512/2082/2082045.png",
+        name: "Lunch",
         userId: 1,
       },
       {
-        icon: 'dinner_icon.png',
-        name: 'Dinner',
+        icon: "https://cdn-icons-png.flaticon.com/128/3274/3274099.png",
+        name: "Dinner",
         userId: 1,
       },
       {
-        icon: 'snack_icon.png',
-        name: 'Snack',
+        icon: "snack_icon.png",
+        name: "Snack",
         userId: 2,
       },
       {
-        icon: 'dessert_icon.png',
-        name: 'Dessert',
+        icon: "dessert_icon.png",
+        name: "Dessert",
         userId: 2,
       },
       {
-        icon: 'brunch_icon.png',
-        name: 'Brunch',
+        icon: "brunch_icon.png",
+        name: "Brunch",
         userId: 2,
       },
       {
-        icon: 'midnight_snack_icon.png',
-        name: 'Midnight Snack',
+        icon: "midnight_snack_icon.png",
+        name: "Midnight Snack",
         userId: 7,
       },
       {
-        icon: 'tea_time_icon.png',
-        name: 'Tea Time',
+        icon: "tea_time_icon.png",
+        name: "Tea Time",
         userId: 8,
       },
       {
-        icon: 'second_breakfast_icon.png',
-        name: 'Second Breakfast',
+        icon: "second_breakfast_icon.png",
+        name: "Second Breakfast",
         userId: 9,
       },
       {
-        icon: 'dinner_icon.png',
-        name: 'Dinner',
+        icon: "dinner_icon.png",
+        name: "Dinner",
         userId: 10,
       },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('meals', null, {});
+    return queryInterface.bulkDelete("meals", null, {});
   },
 };
